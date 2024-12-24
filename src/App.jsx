56224c4858a2c71a777256ce7433ba5a0aa7ef27
Shivananda Sai <shivanandasai.38@gmail.com>
2024-12-24 15:09:47 +0530

    Initialize React application with Vite setup and basic components

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import data from "../behaviorals.json";
+import VideoPlayer from "./VIdeoPlayer";
+function App() {
+  return (
+    <>
+      <h1>Behavioral Interviews</h1>
+      {data.behaviorals.map((item) => {
+        return (
+          <VideoPlayer
+            name={item.name}
+            description={item.description}
+            vimeoId={item.video.vimeoId}
+            instructor={item.video.instructor}
+          />
+        );
+      })}
+    </>
+  );
+}
+
+export default App;
